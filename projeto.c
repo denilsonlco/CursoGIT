3074db9f6443f73bcdd1890e0809ba0e7693e86c
Denilson Oliveira <denilson_luciano@hotmail.com>
2019-06-13 19:52:25 -0300

    Adicionando função soma

---
+++ projeto.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum(int a, int b)
+{
+	int soma_result = a + b;
+	return sum_result;
+}
+
+int main()
+{
+	int num1, num2;
+	printf("Digite o primeiro numero: ");
+	Scanf ("%i", &num1);
+	printf("Digite o segundo numero: ");
+	Scanf ("%i", &num2);
+	printf("Soma: ", sum_result(num1, num2));
+	return 0;
+}
